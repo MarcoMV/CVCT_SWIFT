b8ceabda22addc59b96045a737f76ca7b6d3f378
MarcoMV <markovera93@gmail.com>
2015-05-22 02:41:23 -0500

    Tipos OPTIONAL, conceptos y ejemplo

--- Constantes-Variables/ViewController.swift
+++ Constantes-Variables/ViewController.swift
@@ -36,6 +36,35 @@ class ViewController: UIViewController {
         var a = 23
         var b = Float(a)
         println("Entero: \(a) Float: \(b)")
+        
+        /*String a Int*/
+        var edad = "21"
+        var edadInt = edad.toInt()
+        
+        println("Edad: \(edadInt)")
+        
+        /*Tipos OPTIONAL, conceptos y ejemplos*/
+        //El concepto optional tiene que ver con la protección del dato y evitar que éste se rompa cuando
+        //no existe, en la asignación de valores, es cuando se entregan malos valores ahí es cuando podría
+        //ocurrir un rompimiento de la funcionalidad misma, para esto se propone usar Optional
+        
+        var numeroString2 = "12"
+        var numeroEntero2:Int? = numeroString2.toInt()
+        println("numeroEntero2: \(numeroEntero2)") /*Imprimira la variable como optional*/
+        println("numeroEntero2: \(numeroEntero2!)") /*Con el ! ya no lo hará así pero ya esta desprotegido y se podría romper el programa*/
+        
+        /*Una manera de prevenir aun cuando no este forzado a que no sea optional*/
+        /*Haremos una condicional de validación*/
+        
+        var edad1 = "23"
+        var edadNumero: Int? = edad1.toInt()
+        
+        if let edadNoOptional = edadNumero{
+            println("Edad numero : \(edadNumero!)")
+        }
+        else {
+            println("No existe la conversión")
+        }
     }
 
     override func didReceiveMemoryWarning() {
